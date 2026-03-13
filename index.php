18ec99edae38875e25a27fa3474702362087fcf1
nfloresfnav <26002164@galileo.edu>
2026-03-13 08:15:16 -0600

    Proyecto Php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Página de Nelson Flores</title>
+</head>
+<body>
+    <h1>Hola soy Nelson Flores</h1>
+    <p>Y este es mi proyecto.</p>
+    
+    <!-- Mostrar imagen -->
+    <img src="images.jpeg" alt="Imagen de Nelson Flores" width="300">
+</body>
+</html>
